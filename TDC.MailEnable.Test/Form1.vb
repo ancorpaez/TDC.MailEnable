f521efb227ea6b3f558f2328fc08db680626bf16
Ancor <126503386+ancorpaez@users.noreply.github.com>
2024-03-12 12:03:34 +0000

    Delete TDC.MailEnable.Test directory

--- TDC.MailEnable.Test/Form1.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports TDC.MailEnable.Core
-
-Public Class Form1
-    'Private Test1 = New TDC.MailEnable.Core.Main
-
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        MsgBox(TDC.MailEnable.Core.TestString)
-    End Sub
-End Class
