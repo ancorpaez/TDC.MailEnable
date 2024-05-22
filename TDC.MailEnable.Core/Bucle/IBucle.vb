cca03257cc7877ac2c250448d7f43f8e2fde2d2e
Ancor <126503386+ancorpaez@users.noreply.github.com>
2024-05-22 11:59:52 +0100

    Delete TDC.MailEnable.Core/Bucle/IBucle.vb
    Descartado

--- TDC.MailEnable.Core/Bucle/IBucle.vb
+++
@@ -1,9 +0,0 @@
-﻿Namespace Bucle
-    Public Interface IBucle
-        Sub Inicia()
-        Sub Detener()
-        Property Intervalo As Integer
-        ReadOnly Property GetHashCode As Integer
-        Event Bucle(Sender As Object, ByRef Detener As Boolean)
-    End Interface
-End Namespace
